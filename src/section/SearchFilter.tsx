e9f7561def9b896ecdd84fe6f3aaeb40821bd418
mrhrifat <mrhrifat383759@gmail.com>
2023-10-19 22:33:05 +0600

    refactor: responsive

--- src/section/SearchFilter.tsx
+++ src/section/SearchFilter.tsx
@@ -1,23 +1,23 @@
 const SearchFilter = () => {
   return (
-    <div className="py-10 grid grid-cols-3 gap-8 justify-between items-end">
+    <div className="py-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 justify-between items-end">
       {/* 1st Item - Search */}
-      <div>
+      <div className="max-lg:col-span-2">
         <form action="#">
           <div className="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
             <input
               type="text"
               name="rocketname"
               id="rocketname"
-              className="border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              className="w-full border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
               placeholder="Search by name..."
             />
           </div>
         </form>
       </div>
 
-      <div className="col-start-3 col-span-2 flex flex-col">
-        <div className="flex row-row gap-2 items-center justify-end mb-2">
+      <div className="md:col-start-3 col-span-2 flex flex-col">
+        <div className="flex row-row gap-2 items-center sm:justify-end mb-2">
           <input
             id="upcomingLaunches"
             name="upcomingLaunches"
@@ -33,13 +33,13 @@ const SearchFilter = () => {
             </label>
           </div>
         </div>
-        <div className="flex flex-row gap-4 justify-between">
-          <div>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div className="grow">
             <select
               id="country"
               name="country"
               autoComplete="country-name"
-              className="rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+              className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
             >
               <option selected hidden disabled>
                 By Launch Status
@@ -48,12 +48,12 @@ const SearchFilter = () => {
               <option>Failed</option>
             </select>
           </div>
-          <div>
+          <div className="grow">
             <select
               id="country"
               name="country"
               autoComplete="country-name"
-              className="rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+              className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
             >
               <option selected hidden disabled>
                 By Launch Date
