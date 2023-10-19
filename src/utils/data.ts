728fd9477027d354ef1ebbee1c3bdabeb66b9d62
mrhrifat <mrhrifat383759@gmail.com>
2023-10-19 17:26:18 +0600

    ci: image exp/imp

---
+++ src/utils/data.ts
@@ -0,0 +1,21 @@
+import rocket1 from "../img/rocket_01.svg";
+import rocket2 from "../img/rocket_02.svg";
+import rocket3 from "../img/rocket_03.svg";
+import rocket4 from "../img/rocket_04.svg";
+import rocket5 from "../img/rocket_05.svg";
+import rocket6 from "../img/rocket_06.svg";
+import rocket7 from "../img/rocket_07.svg";
+import rocket8 from "../img/rocket_08.svg";
+import rocket9 from "../img/rocket_09.svg";
+
+export {
+  rocket1,
+  rocket2,
+  rocket3,
+  rocket4,
+  rocket5,
+  rocket6,
+  rocket7,
+  rocket8,
+  rocket9,
+};
