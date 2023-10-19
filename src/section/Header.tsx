027b5c877eb27e3effd4a3a065faf5791c3fe089
mrhrifat <mrhrifat383759@gmail.com>
2023-10-19 15:58:18 +0600

    ci: header component

---
+++ src/section/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="mt-28 text-center">
+      <h2 className="text-4xl">Spaceflight Details</h2>
+      <p className="text-slate-400 mt-1">
+        Find out the elaborate features of all the past big spaceflights.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
