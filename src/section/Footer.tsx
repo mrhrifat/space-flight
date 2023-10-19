67e14ac5e7b74dc923f290f26dad7df3ee1d4e6d
mrhrifat <mrhrifat383759@gmail.com>
2023-10-19 15:58:09 +0600

    ci: footer component

---
+++ src/section/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="text-center">
+      <p className="text-slate-400 mt-5">
+        Created by the brilliant minds behind SpaceX
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
