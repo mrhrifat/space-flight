a6675a7c24490483b6e2ca622411a9f85bb3a9b6
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 01:16:15 +0600

    refactor: context

--- src/section/FlightDetails.tsx
+++ src/section/FlightDetails.tsx
@@ -1,12 +1,15 @@
+import { useContext } from "react";
 import Details from "../components/Details";
+import FlightContext from "../utils/FlightContext";
 
 const FlightDetails = () => {
+  const { itemToDisplay } = useContext(FlightContext);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      <Details />
-      <Details />
-      <Details />
-      <Details />
+      {itemToDisplay.map((item) => (
+        <Details key={item.mission_name} launchData={item} />
+      ))}
     </div>
   );
 };
