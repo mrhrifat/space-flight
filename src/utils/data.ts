e6263f7a67229ebe2dee2ff979c70d0f24f24de4
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 17:21:31 +0600

    build: data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,12 +1,22 @@
-import rocket1 from "../img/rocket_01.svg";
-import rocket2 from "../img/rocket_02.svg";
-import rocket3 from "../img/rocket_03.svg";
-import rocket4 from "../img/rocket_04.svg";
-import rocket5 from "../img/rocket_05.svg";
-import rocket6 from "../img/rocket_06.svg";
-import rocket7 from "../img/rocket_07.svg";
-import rocket8 from "../img/rocket_08.svg";
-import rocket9 from "../img/rocket_09.svg";
+/**
+ * Title: data
+ * Description:
+ * Filename: data.ts
+ * Path: /src/utils/data.ts
+ * Author: Mrh Rifat (Programmer)
+ * Date: Oct 21, 2023
+ *
+ */
+
+import rocket1 from "../img/rocket_01.png";
+import rocket2 from "../img/rocket_02.png";
+import rocket3 from "../img/rocket_03.png";
+import rocket4 from "../img/rocket_04.png";
+import rocket5 from "../img/rocket_05.png";
+import rocket6 from "../img/rocket_06.png";
+import rocket7 from "../img/rocket_07.png";
+import rocket8 from "../img/rocket_08.png";
+import rocket9 from "../img/rocket_09.png";
 
 export {
   rocket1,
@@ -18,4 +28,44 @@ export {
   rocket7,
   rocket8,
   rocket9,
+};
+
+export const DOTS = "...";
+
+export const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const defaultFetchCurrentData = async () => {
+  return [];
+};
+
+// Context Default Value
+export const defaultValue = {
+  currentData: [],
+  setCurrentData: defaultFetchCurrentData,
+  currentPage: 1,
+  setCurrentPage: (): number => 1,
+  totalItems: 0,
+  upcoming: false,
+  setUpcoming: (): boolean => false,
+  query: "",
+  setQuery: (): string => "",
+  filter: "all",
+  setFilter: (): string => "all",
+  itemsPerpage: 9,
+  startIndex: 1,
+  endIndex: 1,
+  itemToDisplay: [],
 };
