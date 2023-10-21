52ec23f56c1cc5089ec98cd0d136aec7bb214dba
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 13:00:27 +0600

    build: section import & export

---
+++ src/section/index.ts
@@ -0,0 +1,8 @@
+import Error from "./Error";
+import FlightDetails from "./FlightDetails";
+import Footer from "./Footer";
+import Header from "./Header";
+import Pagination from "./Pagination";
+import SearchAndFilter from "./SearchAndFilter";
+
+export { Error, FlightDetails, Footer, Header, Pagination, SearchAndFilter };
