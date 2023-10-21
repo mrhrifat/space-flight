944708cdb1c4318d6d90f9934cfaabb02aa8f662
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 12:50:53 +0600

    refactor: accessbility & others

--- src/components/FilterStatusDate.tsx
+++ src/components/FilterStatusDate.tsx
@@ -1,64 +1,103 @@
 import { ChangeEvent, useContext } from "react";
 import FlightContext from "../utils/FlightContext";
+import { useNavigate } from "react-router-dom";
 
 const FilterStatusDate = () => {
   const { setFilter } = useContext(FlightContext);
+  const navigate = useNavigate();
 
   const handleLaunchStatus = (e: ChangeEvent<HTMLSelectElement>) => {
     switch (e.target.value) {
       case "all":
-        return setFilter("all");
+        setFilter("all");
+        navigate("/1");
+        break;
       case "successfull":
-        return setFilter("successfull");
+        setFilter("successfull");
+        navigate("/1");
+        break;
       case "failed":
-        return setFilter("failed");
+        setFilter("failed");
+        navigate("/1");
+        break;
       case "lastWeek":
-        return setFilter("lastWeek");
+        setFilter("lastWeek");
+        navigate("/1");
+        break;
       case "lastMonth":
-        return setFilter("lastMonth");
+        setFilter("lastMonth");
+        navigate("/1");
+        break;
       case "lastYear":
-        return setFilter("lastYear");
+        setFilter("lastYear");
+        navigate("/1");
+        break;
       case "lastFiveYear":
-        return setFilter("lastFiveYear");
+        setFilter("lastFiveYear");
+        navigate("/1");
+        break;
       default:
-        return setFilter("all");
+        setFilter("all");
+        navigate("/1");
+        break;
     }
   };
 
   return (
     <div className="flex flex-col sm:flex-row gap-4">
+      {/* Filter By Launch Status */}
+
       <div className="grow">
+        <label htmlFor="byStatus" className="sr-only">
+          Launch Status
+        </label>
         <select
-          id="country"
-          name="country"
-          autoComplete="country-name"
+          id="byStatus"
+          name="byStatus"
           className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-          defaultValue={"none"}
+          defaultValue="none"
           onChange={handleLaunchStatus}
         >
-          <option value="none" hidden disabled>
+          <option value="none" hidden disabled key="1">
             By Launch Status
           </option>
-          <option value="successfull">Successfull</option>
-          <option value="failed">Failed</option>
+          <option value="successfull" key="2">
+            Successfull
+          </option>
+          <option value="failed" key="3">
+            Failed
+          </option>
         </select>
       </div>
+
+      {/* Filter By Launch Date & Time */}
+
       <div className="grow">
+        <label htmlFor="byLaunchDate" className="sr-only">
+          Launch Date
+        </label>
         <select
-          id="country"
-          name="country"
-          autoComplete="country-name"
+          id="byLaunchDate"
+          name="byLaunchDate"
           className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-          defaultValue={"none"}
+          defaultValue="none"
           onChange={handleLaunchStatus}
         >
-          <option value="none" hidden disabled>
+          <option value="none" hidden disabled key="1">
             By Launch Date
           </option>
-          <option value="lastWeek">Last week</option>
-          <option value="lastMonth">Last month</option>
-          <option value="lastYear">Last year</option>
-          <option value="lastFiveYear">Last 5 year</option>
+          <option value="lastWeek" key="2">
+            Last week
+          </option>
+          <option value="lastMonth" key="3">
+            Last month
+          </option>
+          <option value="lastYear" key="4">
+            Last year
+          </option>
+          <option value="lastFiveYear" key="5">
+            Last 5 year
+          </option>
         </select>
       </div>
     </div>
