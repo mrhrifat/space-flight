c8fdf01613b7d896d5b0e57908345ff16731c7d0
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 18:19:28 +0600

    refactor: loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import Footer from "./section/Footer";
+import { Footer, Loading } from "./section";
 const Header = lazy(() => import("./section/Header"));
 const FlightDetails = lazy(() => import("./section/FlightDetails"));
 const SearchAndFilter = lazy(() => import("./section/SearchAndFilter"));
@@ -8,22 +8,22 @@ const Pagination = lazy(() => import("./section/Pagination"));
 const App = () => {
   return (
     <main className="container mx-auto max-sm:px-10">
-      <Suspense fallback={"Loading"}>
+      <Suspense fallback={<Loading />}>
         <header>
           <Header />
         </header>
       </Suspense>
-      <Suspense fallback={"Loading"}>
+      <Suspense fallback={<Loading />}>
         <div>
           <SearchAndFilter />
         </div>
       </Suspense>
-      <Suspense fallback={"Loading"}>
+      <Suspense fallback={<Loading />}>
         <div>
           <FlightDetails />
         </div>
       </Suspense>
-      <Suspense fallback={"Loading"}>
+      <Suspense fallback={<Loading />}>
         <div>
           <Pagination />
         </div>
