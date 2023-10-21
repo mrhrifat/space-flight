b80ab84505a84d7d2fc8310ef495a1e8c5fd7843
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 17:02:23 +0600

    refactor: update text

--- src/section/Footer.tsx
+++ src/section/Footer.tsx
@@ -11,9 +11,7 @@
 const Footer = () => {
   return (
     <div className="text-center">
-      <p className="text-slate-500 mt-5">
-        Created by the brilliant minds behind SpaceX
-      </p>
+      <p className="text-slate-500 mt-5">Created by Mrh Rifat</p>
     </div>
   );
 };
