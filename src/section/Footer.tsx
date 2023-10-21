f9e47da0200ea125c38ca32c85e37ce5aee30d51
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 13:00:04 +0600

    refactor: accessbility

--- src/section/Footer.tsx
+++ src/section/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="text-center">
-      <p className="text-slate-400 mt-5">
+      <p className="text-slate-500 mt-5">
         Created by the brilliant minds behind SpaceX
       </p>
     </div>
