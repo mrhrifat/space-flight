5c4e98a9233dc64818d60a0cc0ce52998b46f5a5
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 21:22:59 +0600

    build: env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.VITE_BASE_URL_LN": JSON.stringify(
+      process.env.VITE_BASE_URL_LN
+    ),
+  },
+});
