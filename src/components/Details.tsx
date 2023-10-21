a64247f4d2c8b22102a9ae33c5acd92928ffb116
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 12:49:23 +0600

    refactor: accessbility

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -3,13 +3,13 @@ import { LaunchDataType } from "../utils/type";
 
 const Details = ({ launchData }: { launchData: LaunchDataType }) => {
   const badgeSuccess = (
-    <div className="bg-green-600 rounded-md text-center text-white py-1/4 px-3 mx-auto">
+    <div className="bg-[#0B652F] font-bold leading-5 rounded-md text-center text-white py-1 px-3 mx-auto">
       Success
     </div>
   );
 
   const badgeFailed = (
-    <div className="bg-red-600 rounded-md text-center text-white py-1/4 px-3 mx-auto">
+    <div className="bg-[#B01212] font-bold leading-5 rounded-md text-center text-white py-1 px-3 mx-auto">
       Failed
     </div>
   );
@@ -20,19 +20,19 @@ const Details = ({ launchData }: { launchData: LaunchDataType }) => {
         <img src={rocketImage()} alt="rocket1" className="w-24 h-24" />
       </div>
       <div className="text-center mb-4">
-        <p className="text-slate-400">
+        <p className="text-slate-500">
           Launch Date:
-          <span className="text-slate-500">
+          <span className="text-slate-700">
             {isoDateToLocalDate(launchData.launch_date_utc)}
           </span>
         </p>
         <h3 className="text-2xl mt-2">{launchData.mission_name}</h3>
-        <small className="text-slate-400">
+        <small className="text-slate-500">
           {launchData.rocket.rocket_name}
         </small>
       </div>
       <div>
-        <p className="text-slate-400">Launch Status:</p>
+        <p className="text-slate-500 mb-2">Launch Status:</p>
 
         {launchData.launch_success === true ? badgeSuccess : badgeFailed}
       </div>
