8f46afd94b260ec3a3276e648d1e5dfcb8804a93
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 13:00:09 +0600

    refactor: accessbility

--- src/section/Header.tsx
+++ src/section/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="mt-28 text-center">
       <h2 className="text-4xl">Spaceflight Details</h2>
-      <p className="text-slate-400 mt-1">
+      <p className="text-slate-500 mt-1">
         Find out the elaborate features of all the past big spaceflights.
       </p>
     </div>
