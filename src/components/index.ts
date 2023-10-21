c6d446b572478bc5df640b5080459afc616a4066
mrhrifat <mrhrifat383759@gmail.com>
2023-10-21 12:51:16 +0600

    build: all components import export

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Details from "./Details";
+import FilterStatusDate from "./FilterStatusDate";
+import Search from "./Search";
+import Upcoming from "./Upcoming";
+import usePagination from "./usePagination";
+
+export { Details, FilterStatusDate, Search, Upcoming, usePagination };
